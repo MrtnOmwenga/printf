412b52bdc38252980f10416d1cbf038d4a5a0f0c
Lekarri <lekarrikaila567@gmail.com>
2022-10-19 09:23:50 -0700

    reconfigured main.h

--- main.h
+++ main.h
@@ -4,18 +4,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-/* print_nums */
-int print_int(va_list l, flags_t *f);
-void print_number(int n);
-int print_unsigned(va_list l, flags_t *f);
-int count_digit(int i);
-
-/* print_bases */
-int print_hex(va_list l, flags_t *f);
-int print_binary(va_list l, flags_t *f);
-int print_hex_big(va_list l, flags_t *f);
-int print_octal(va_list l, flags_t *f);
-
 /* converter */
 char *convert(unsigned long int num, int base, int lowercase);
 
